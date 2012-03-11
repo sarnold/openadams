5e351bfec1eb5d1b228570fc2bfb49fe2e3b8bf1
achimk <achimk@db38004f-20e5-4a32-9cac-6ed1e75b6bac>
2012-03-11 15:15:16 +0000

    fixed minor bug

--- build.py
+++ build.py
@@ -17,8 +17,8 @@ SOURCES = "_naf_usecase.py _naf_tree.py _naf_tableview.py _naf_requirement.py "\
     "_naf_itemmodel.py _naf_filter.py _naf_commons.py _naf_component.py _naf_database.py _naf_feature.py "\
     "_naf_folder.py _naf_image.py _naf_imageviewer.py _naf_simplesection.py "\
     "_naf_testcase.py _naf_testsuite.py _naf_textviewer.py naf_exportxml.py naf_editor.py " \
-    "naf_updatedb_v1_to_v2.py _naf_about.py _oatr_database.py _oatr_tableview.py filepicker.py"\
-    "oaxml2db.py _oatr_importwizard.py _oatr_testrun.py _oatr_commons.py _oatr_testsuite.py"
+    "naf_updatedb_v1_to_v2.py _naf_about.py _oatr_database.py _oatr_tableview.py filepicker.py "\
+    "oaxml2db.py _oatr_importwizard.py _oatr_testrun.py _oatr_commons.py _oatr_testsuite.py "
 DATAFILES = 'nafms_de.qm oareport.css oareport.xsl oareportlang.xsl README.txt CHANGELOG.txt COPYING.txt'
 
 TRANSLATIONSOURCES = SCRIPTS + SOURCES + '_nafhelp_translationsstrings.py'
