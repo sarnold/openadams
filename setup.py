9c338291d093f0385c5219e63242e36b0709fe8d
S. Lockwood-Childs <sjl@vctlabs.com>
2016-01-13 01:03:51 -0800

    generate _naf_resources.py in setup.py
    _naf_resources.py was another file that the old build.py
    used to generate so now setup.py must do so

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ SCRIPTS = ["oaeditor.py", "oalogview.py", "oatestrunner.py",
            "_naf_requirement.py", "_naf_itemmodel.py", "_naf_filter.py", 
            "_naf_commons.py", "_naf_component.py", "_naf_database.py", 
            "_naf_feature.py", "_naf_folder.py", "_naf_image.py", 
-           "_naf_imageviewer.py", "_naf_simplesection.py", 
+           "_naf_imageviewer.py", "_naf_simplesection.py", "_naf_resources.py",
            "_naf_testcase.py", "_naf_testsuite.py", "_naf_textviewer.py", 
            "_naf_about.py", "_oatr_database.py", "_oatr_tableview.py", 
            "_oatr_importwizard.py", "_oatr_testrun.py", "_oatr_commons.py", 
@@ -19,8 +19,11 @@ SCRIPTS = ["oaeditor.py", "oalogview.py", "oatestrunner.py",
 DATAFILES = ["nafms_de.qm", "oareport.css", "oareport.xsl", "oareportlang.xsl",
              "README.txt", "CHANGELOG.txt", "COPYING.txt" ]
 
-# build binary version of Qt translation ile
+# build binary version of Qt translation file
 subprocess.call(["lrelease", "nafms_de.ts"])
+
+# build PyQt resources
+subprocess.call(["pyrcc4", "-py2", "-o", "_naf_resources.py", "nafms.qrc"])
 
 # update version file
 subprocess.call(["./update_version.sh"])
